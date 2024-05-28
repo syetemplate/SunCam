b702b3242e8efb8d6283a25b39bcc3155c1a4697
SaharAvr <Sahar014g@gmail.com>
2024-05-28 05:39:53 +0300

    fix header logo link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -50,7 +50,7 @@ const Header = () => {
     return (
         <header className="bg-white text-gray-600 body-font lg:px-28 lg:py-4 border-b border-green-200 border-opacity-50" ref={headerRef}>
             <div className="container mx-auto flex flex-wrap p-5 flex-row items-center justify-between">
-                <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                <a href="/" className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
                     <img src={logoDark.src} alt="logo" />
                 </a>
                 <div className="md:hidden">
