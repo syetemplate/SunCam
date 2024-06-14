f257b9c3d22fa69ad5738bdfe63e9d11788cda74
SaharAvr <Sahar014g@gmail.com>
2024-06-14 22:09:06 +0300

    refactor facts count animation

--- src/components/Facts.jsx
+++ src/components/Facts.jsx
@@ -1,17 +1,41 @@
 'use client';
 
 import React from 'react';
-import { useInView } from 'react-intersection-observer';
 import CountUp from 'react-countup';
+import { throttle } from 'lodash';
 import clsx from 'clsx';
 import factsBgImage from '@/assets/bg/facts.jpg';
 import content from '@/content';
 
 const Facts = ({ className }) => {
-    const [ref, inView] = useInView({
-        triggerOnce: true,
-        threshold: 0.5,
-    });
+    const factsContainerRef = React.useRef(null);
+    const [isInView, setIsInView] = React.useState(false);
+
+    const updateIsInView = () => {
+        const isCurrentlyInView = (() => {
+            if (!factsContainerRef.current) {
+                return;
+            }
+            const { top, bottom } = factsContainerRef.current.getBoundingClientRect();
+            return (top < window.innerHeight && bottom > 0)
+        })();
+        if (!isCurrentlyInView) {
+            return;
+        }
+        setIsInView(true);
+    };
+
+    const initIsInView = () => {
+        const throttledUpdateIsInView = throttle(updateIsInView, 100);
+        window.addEventListener('scroll', throttledUpdateIsInView);
+        throttledUpdateIsInView();
+
+        return () => {
+            window.removeEventListener('scroll', throttledUpdateIsInView);
+        };
+    };
+
+    React.useEffect(initIsInView, []);
 
     return (
         <section
@@ -22,7 +46,6 @@ const Facts = ({ className }) => {
                 backgroundPosition: 'center',
                 backgroundSize: 'cover',
             }}
-            ref={ref}
         >
             <div className="container mx-auto z-10 px-4 lg:px-28">
                 <div className="flex flex-wrap justify-center">
@@ -33,7 +56,7 @@ const Facts = ({ className }) => {
                         </div>
                     </div>
                 </div>
-                <div className="flex flex-wrap">
+                <div className="flex flex-wrap" ref={factsContainerRef}>
                     {content.facts.items.map((fact, index) => (
                         <div key={index} className="lg:w-1/4 pr-4 pl-4 sm:w-1/2 w-full pr-4 pl-4">
                             <div className="single-fact text-center mb-50">
@@ -44,7 +67,7 @@ const Facts = ({ className }) => {
                                     <h5>
                                         <CountUp
                                             start={0}
-                                            end={inView ? fact.value : 0}
+                                            end={isInView ? fact.value : 0}
                                             duration={2}
                                             separator=","
                                             decimal="."
