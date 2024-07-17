6b550b77980661015e94d4e9816f0b1d4dcbbfa5
Yuval <yulev5@gmail.com>
2024-07-17 23:38:20 +0300

    change env variable names

--- src/utils/email.js
+++ src/utils/email.js
@@ -1,9 +1,9 @@
 import emailjs from 'emailjs-com';
 
 const sendEmail = async ({ name, phone, email, to_email }) => {
-    const serviceID = process.env.NEXT_PUBLIC_SERVICE;
-    const templateID = process.env.NEXT_PUBLIC_TEMPLATE;
-    const userID = process.env.NEXT_PUBLIC_USER;
+    const serviceID = process.env.EMAILJS_SERVICE_ID;
+    const templateID = process.env.EMAILJS_TEMPLATE_ID;
+    const userID = process.env.EMAILJS_USER_ID;
 
     const templateParams = {
         to_name: 'Website Owner',
